a2e6e671887bee23e09156506b58813631a7ca9b
canalem <marc.canale@hotmail.com>
2020-12-04 21:18:22 +0000

    Initialise search module.

--- clusterpluck/search.py
+++ clusterpluck/search.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from astroquery.gaia import Gaia
 
 def search(ra_coord, dec_coord, radius):
@@ -6,6 +7,5 @@ def search(ra_coord, dec_coord, radius):
                WHERE CONTAINS(POINT('ICRS',gaiadr2.gaia_source.ra,gaiadr2.gaia_source.dec),CIRCLE('ICRS',{ra},{dec},{area}))=1 \
                AND bp_rp IS NOT NULL ;""".format(ra=str(ra_coord), dec=str(dec_coord), area=str(radius))
     job1 = Gaia.launch_job_async(query)
-    search_results = job1.get_results()
-    return search_results
-
+    raw_table = job1.get_results()
+    return raw_table
